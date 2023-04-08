92c530b26ddebdccbb27157019828a5acc9bcce2
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2023-04-08 12:26:13 +0600

    feat: add process manager

---
+++ cameras/manager.py
@@ -0,0 +1,26 @@
+''' Manager file for managing camera operations '''
+import subprocess
+
+
+class ProcessManager:
+    ''' Class representing process manager '''
+
+    def __init__(self, path: str = 'script.py'):
+        self.process: None | subprocess.Popen = None
+        self.script: str = path
+
+    def run(self) -> None:
+        ''' Runs the process '''
+        if not self.is_running:
+            self.process = subprocess.Popen(['python', self.script])
+
+    def terminate(self) -> None:
+        ''' Terminates the process '''
+        if self.is_running:
+            self.process.terminate()
+            self.process = None
+
+    @property
+    def is_running(self) -> bool:
+        ''' Returns True if the process is running '''
+        return self.process is not None
