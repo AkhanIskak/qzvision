fa26707ea55d2ea3b69ecba8d5129b4a8146c7c8
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2023-04-09 03:42:43 +0600

    feat: add camera turning on/off

--- cameras/script.py
+++ cameras/script.py
@@ -1,23 +1,38 @@
 ''' Script file for work of cameras '''
+from requests import post
 import signal
-import time
 import sys
 
 from logger import logger
+from camera import Camera
+import config
+
+
+camera = Camera(device=config.Vision.DEVICE.value,
+                video=config.Vision.VIDEO.value)
 
 
 def turn_off_handler(*_):
     ''' Turn off signal handler '''
     logger.info('Turning camera off')
+    delta = camera.deactivate()
+
+    post(
+        url=config.Server.URL.value,
+        json={
+            'passengerAmount': delta,
+            'routeId': config.Bus.ROUTE.value,
+            'specificBusId': config.Bus.ID.value
+        }
+    )
+
     sys.exit(0)
 
 
 def turn_on_handler():
     ''' Turning on cameras '''
     logger.info('Turning camera on')
-
-    while True:
-        time.sleep(1)
+    camera.activate()
 
 
 signal.signal(signal.SIGTERM, turn_off_handler)
