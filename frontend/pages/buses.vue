994617a955948faac7d4e2923958d41e1d2fa821
AkhanIzkak <akhanizkak@gmail.com>
2023-04-09 05:44:44 +0600

    buses

---
+++ frontend/pages/buses.vue
@@ -0,0 +1,32 @@
+<template>
+  <container>
+    <h2>Загруженность автобусов</h2>
+    <hr>
+    <ul v-for="route in Object.keys(buses)" :key="route">
+     <h5> Номер автобуса:{{route}}</h5>
+      <li v-for="bus in buses[route]" :key="bus">Айди автобуса:{{bus.specificBusId}} . Количество людей: {{bus.passengerAmount}} . Загруженность:{{bus.passengerAmount<30?"Низкая":bus.passengerAmount<50?"Средняя":"Очень Высокая"}}</li>
+      <hr>
+    </ul>
+
+  </container>
+</template>
+
+<script>
+
+export default {
+  name: 'BusesPage',
+
+  async asyncData() {
+    let response = await fetch('https://blizhe.kz/api/buses')
+    response = await response.json()
+    console.log(response)
+    return {
+      buses: response
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
