94028996575e059ac158de9cdb8a963979fb22b9
AkhanIzkak <akhanizkak@gmail.com>
2023-04-09 05:25:33 +0600

    fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '..', 'static'));
   app.setGlobalPrefix('api');
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
