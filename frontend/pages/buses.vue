a8fe752faccd6eceeebd5da2af51190da56c4f3a
AkhanIzkak <akhanizkak@gmail.com>
2023-04-09 06:12:08 +0600

    fix

--- frontend/pages/buses.vue
+++ frontend/pages/buses.vue
@@ -1,12 +1,15 @@
 <template>
   <container>
     <h2>Загруженность автобусов</h2>
-<!--    <hr>-->
-<!--    <ul v-for="route in Object.keys(buses)" :key="route">-->
-<!--     <h5> Номер автобуса:{{route}}</h5>-->
-<!--      <li v-for="bus in buses[route]" :key="bus">Айди автобуса:{{bus.specificBusId}} . Количество людей: {{bus.passengerAmount}} . Загруженность:{{bus.passengerAmount<30?"Низкая":bus.passengerAmount<50?"Средняя":"Очень Высокая"}}</li>-->
-<!--      <hr>-->
-<!--    </ul>-->
+    <hr>
+    <ul v-for="route in Object.keys(buses)" :key="route">
+      <h5> Номер автобуса:{{ route }}</h5>
+      <li v-for="bus in buses[route]" :key="bus">Айди автобуса:{{ bus.specificBusId }} . Количество людей:
+        {{ bus.passengerAmount }} .
+        Загруженность:{{ bus.passengerAmount < 30 ? "Низкая" : bus.passengerAmount < 50 ? "Средняя" : "Очень Высокая" }}
+      </li>
+      <hr>
+    </ul>
 
   </container>
 </template>
@@ -15,19 +18,15 @@
 
 export default {
   name: 'BusesPage',
-  async mounted() {
-      let response = await fetch(window.location.origin+'/api/buses')
-      response = await response.json()
-      console.log(response)
+
+  async asyncData() {
+    let response = await fetch(window.location.origin + '/api/buses')
+    response = await response.json()
+    console.log(response)
+    return {
+      buses: response
+    }
   }
-  // async asyncData() {
-  //   let response = await fetch('api/buses')
-  //   response = await response.json()
-  //   console.log(response)
-  //   return {
-  //     buses: response
-  //   }
-  // }
 }
 </script>
 
